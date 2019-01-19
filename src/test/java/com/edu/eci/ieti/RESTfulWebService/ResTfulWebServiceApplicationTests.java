04a9232f0dc071fba031649d3267aef92f6fa191
Sebastián Reyes <sebastianreyes01@hotmail.com>
2019-01-19 17:20:26 -0500

    RESTful Web Service review

---
+++ src/test/java/com/edu/eci/ieti/RESTfulWebService/ResTfulWebServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.edu.eci.ieti.RESTfulWebService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResTfulWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
